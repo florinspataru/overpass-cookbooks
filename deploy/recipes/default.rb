7d0c1a3d0f982985438d681b96c02e38c4b8ade5
Florin Spataru <florin.spataru@arobs.com>
2014-11-13 19:32:55 +0200

    Modify deploy method

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -6,8 +6,9 @@ node[:deploy].each do |application, deploy|
     message "The app will be deployed to: #{application} :: #{deploy.inspect}"
     level :info
   end
-  opsworks_deploy_user do
+  opsworks_deploy do
     deploy_data deploy
+    app application
   end
 
 end
