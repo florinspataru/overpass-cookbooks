480b5b418ec4a0146e8f0df12addef228b16f756
Florin Spataru <florin.spataru@arobs.com>
2014-11-13 19:20:38 +0200

    Add log on deploy::default

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -3,7 +3,7 @@ include_recipe 'dependencies'
 node[:deploy].each do |application, deploy|
 
   log "message" do
-    message "This is the message that will be added to the log."
+    message "The app will be deployed to: #{application} :: #{deploy.inspect}"
     level :info
   end
   opsworks_deploy_user do
